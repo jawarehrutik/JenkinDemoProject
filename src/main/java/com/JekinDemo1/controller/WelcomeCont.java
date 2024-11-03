a0e240d2a44dc563c66aa6f34f0a4a2f4f49039b
jawarehrutik <jawarehrutik1999@gmail.com>
2024-11-03 17:40:09 +0530

    first commit

---
+++ src/main/java/com/JekinDemo1/controller/WelcomeCont.java
@@ -0,0 +1,17 @@
+package com.JekinDemo1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeCont {
+	
+	@GetMapping("/welcome")
+	public String getWelcome()
+	{
+		return "Welcome from controller";
+	}
+	
+
+}
